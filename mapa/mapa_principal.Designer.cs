e4eed73a6d7c59aa9ac5bc1ee88fd73a52f2c6da
kaog2 <kvin.ortega@gmail.com>
2018-05-10 14:34:08 +0200

    Agregar archivos de proyecto.

---
+++ mapa/mapa_principal.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace mapa
+{
+    partial class mapa_principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mapa = new GMap.NET.WindowsForms.GMapControl();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.txtlatitud = new System.Windows.Forms.TextBox();
+            this.txtlongitud = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Mapa
+            // 
+            this.Mapa.Bearing = 0F;
+            this.Mapa.CanDragMap = true;
+            this.Mapa.EmptyTileColor = System.Drawing.Color.Navy;
+            this.Mapa.GrayScaleMode = false;
+            this.Mapa.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.Mapa.LevelsKeepInMemmory = 5;
+            this.Mapa.Location = new System.Drawing.Point(13, 13);
+            this.Mapa.MarkersEnabled = true;
+            this.Mapa.MaxZoom = 2;
+            this.Mapa.MinZoom = 2;
+            this.Mapa.MouseWheelZoomEnabled = true;
+            this.Mapa.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.Mapa.Name = "Mapa";
+            this.Mapa.NegativeMode = false;
+            this.Mapa.PolygonsEnabled = true;
+            this.Mapa.RetryLoadTile = 0;
+            this.Mapa.RoutesEnabled = true;
+            this.Mapa.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.Mapa.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.Mapa.ShowTileGridLines = false;
+            this.Mapa.Size = new System.Drawing.Size(608, 409);
+            this.Mapa.TabIndex = 0;
+            this.Mapa.Zoom = 0D;
+            this.Mapa.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Mapa_MouseDoubleClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(721, 182);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(630, 182);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.addButton);
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(630, 45);
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(147, 20);
+            this.txtdescripcion.TabIndex = 3;
+            // 
+            // txtlatitud
+            // 
+            this.txtlatitud.Location = new System.Drawing.Point(630, 95);
+            this.txtlatitud.Name = "txtlatitud";
+            this.txtlatitud.Size = new System.Drawing.Size(147, 20);
+            this.txtlatitud.TabIndex = 4;
+            // 
+            // txtlongitud
+            // 
+            this.txtlongitud.Location = new System.Drawing.Point(630, 144);
+            this.txtlongitud.Name = "txtlongitud";
+            this.txtlongitud.Size = new System.Drawing.Size(147, 20);
+            this.txtlongitud.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(627, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Descripcion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Latitud";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Longitud";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(630, 220);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(166, 182);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.SeleccionarRegistro);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 434);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtlongitud);
+            this.Controls.Add(this.txtlatitud);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.Mapa);
+            this.Name = "Form1";
+            this.Text = "Mapa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl Mapa;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.TextBox txtlatitud;
+        private System.Windows.Forms.TextBox txtlongitud;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ mapa/mapa_principal.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+
+namespace mapa
+{
+    public partial class mapa_principal : Form
+    {
+
+        GMarkerGoogle marker;
+        GMapOverlay markerOverlay;
+        DataTable dt; // almacenar direcciones
+
+        int filaSeleccionada = 0;
+        //Coordenadas iniciales (obtener por GPS)
+        double LatInicial = -35.4263992;
+        double LngInicial = -71.6554184
+            ;
+
+
+        public mapa_principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dt = new DataTable();
+            dt.Columns.Add(new DataColumn("Descripcion",typeof(string)));
+            dt.Columns.Add(new DataColumn("Lat", typeof(string)));
+            dt.Columns.Add(new DataColumn("Long", typeof(string)));
+
+            // Insertar datos al dt para mostrar en la lista
+
+            dt.Rows.Add("Ubicacion 1", LatInicial, LngInicial);
+            dataGridView1.DataSource = dt;
+
+            //desactivar las columnas de lat y long
+
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+
+            Mapa.DragButton = MouseButtons.Left; // to move maps with mause left click
+            Mapa.CanDragMap = true; // to drag map
+            Mapa.MapProvider = GMapProviders.GoogleMap;
+            Mapa.Position = new PointLatLng(LatInicial, LngInicial);
+            Mapa.MinZoom = 0;
+            Mapa.MaxZoom = 24;
+            Mapa.Zoom = 9;
+            Mapa.AutoScroll = true; // scroll for the maps
+
+            // Marker
+
+            markerOverlay = new GMapOverlay("Marcador"); //agrega un marcador en una capa encima del mapa que generamos
+            marker = new GMarkerGoogle(new PointLatLng(LatInicial,LngInicial),GMarkerGoogleType.green);
+            markerOverlay.Markers.Add(marker); //to add to maps
+
+            //agregar un tooltip de texto a los marcadores
+            marker.ToolTipMode = MarkerTooltipMode.Always;
+            marker.ToolTipText = string.Format("Ubicacion: \n Latitud:{0} \n Longitud: {1}", LatInicial, LngInicial);
+
+            //ahora agregaremos al mapa el marcador al mapa control
+            Mapa.Overlays.Add(markerOverlay);
+
+
+         
+        }
+
+        private void SeleccionarRegistro(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            filaSeleccionada = e.RowIndex; //Fila seleccionada
+            // Recuperamos los datos del grid y los asignamos a los textbox
+            txtdescripcion.Text = dataGridView1.Rows[filaSeleccionada].Cells[0].Value.ToString();
+            txtlatitud.Text = dataGridView1.Rows[filaSeleccionada].Cells[1].Value.ToString(); 
+            txtlongitud.Text = dataGridView1.Rows[filaSeleccionada].Cells[2].Value.ToString();
+
+            // se asignan los valores del grid al marcador
+
+            marker.Position = new PointLatLng(Convert.ToDouble(txtlatitud.Text), Convert.ToDouble(txtlongitud.Text));
+
+            // se posiciona en el foco del mapa en ese punto
+            Mapa.Position = marker.Position;
+
+        }
+
+        private void Mapa_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // se obtienen los datos de lat y lng del mapa donde el User presiona
+            double lat = Mapa.FromLocalToLatLng(e.X, e.Y).Lat;
+            double lng = Mapa.FromLocalToLatLng(e.X, e.Y).Lng;
+
+            // se posicionan en el txt de la lat y long
+
+            txtlatitud.Text = lat.ToString();
+            txtlongitud.Text = lng.ToString();
+
+            // Creamos el marcador para moverlo al lugar indicado
+
+            marker.Position = new PointLatLng(lat, lng);
+            //Tambien se agrega el mensaje al marcador tooltip
+
+            marker.ToolTipText = string.Format("Ubicacion: \n Latitud:{0} \n Longitud: {1}", lat, lng);
+
+        }
+
+        private void addButton(object sender, EventArgs e)
+        {
+            dt.Rows.Add(txtdescripcion.Text, txtlatitud.Text, txtlongitud.Text);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(filaSeleccionada);
+        }
+    }
+}
